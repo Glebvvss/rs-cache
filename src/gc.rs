37edf96b74e900996b56792bd2670061b395eeca
Gleb Vasylenko <hleb.v@platinumlist.pro>
2022-06-03 14:38:12 +0300

    Lifes rework (solve RwLock overusing problem)

--- src/gc.rs
+++ src/gc.rs
@@ -26,71 +26,64 @@ impl Gc {
 }
 
 pub struct Lifes {
-    vec:  RwLock<Vec<(String, u32)>>,
-    map:  RwLock<HashMap<String, (u32, u32)>>,
-    free: RwLock<Vec<u32>>
+    vec:  Vec<(String, u32)>,
+    map:  HashMap<String, (u32, u32)>,
+    free: Vec<u32>
 }
 
 impl Lifes {
     pub fn new() -> Self {
         Lifes {
-            vec:  RwLock::new(Vec::new()),
-            map:  RwLock::new(HashMap::new()),
-            free: RwLock::new(Vec::new())
+            vec:  Vec::new(),
+            map:  HashMap::new(),
+            free: Vec::new()
         }
     }
 
     pub fn grabbed(&self, key: &String) -> bool {
-        let map = self.map.read().unwrap();
-        if map.is_empty() {
+        if self.map.is_empty() {
             return false;
         }
 
-        match map.get(key) {
+        match self.map.get(key) {
             Some(_) => true,
             None    => false
         }
     }
 
     pub fn expire_at(&self, key: &String) -> Option<u32> {
-        let map = self.map.read().unwrap();
-        if map.is_empty() {
+        if self.map.is_empty() {
             return None;
         }
 
-        match map.get(key) {
+        match self.map.get(key) {
             Some((_, expiration)) => Some(expiration.clone()),
             None => None
         }
     }
 
-    pub fn grab(&self, key: &String, expiration: u32) {
-        let mut map  = self.map.write().unwrap();
-        let mut vec  = self.vec.write().unwrap();
-        let mut free = self.free.write().unwrap();
-        match map.get(key) {
+    pub fn grab(&mut self, key: &String, expiration: u32) {
+        match self.map.get(key) {
             Some((mut position, _)) => {
-                map.insert(key.to_string(), (position, expiration));
+                self.map.insert(key.to_string(), (position, expiration));
             },
             None => {
-                let position = match free.pop() {
+                let position = match self.free.pop() {
                     Some(position) => position,
-                    None           => vec.len() as u32
+                    None           => self.vec.len() as u32
                 };
 
-                map.insert(key.to_string(), (position, expiration));
-                vec.insert(position as usize, (key.to_string(), expiration));
+                self.map.insert(key.to_string(), (position, expiration));
+                self.vec.insert(position as usize, (key.to_string(), expiration));
             }
         };
     }
 
-    pub fn release(&self, key: &String) {
-        let mut map  = self.map.write().unwrap();
-        let mut free = self.free.write().unwrap();
-        if let Some((pos, _)) = map.get(key) {
+    pub fn release(&mut self, key: &String) {
+        if let Some((pos, _)) = self.map.get(key) {
             let mut position = pos.clone();
-            map.remove(key);
-            free.push(position);
+            self.map.remove(key);
+            self.free.push(position);
         }
     }
 }
