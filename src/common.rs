f6d7cc1af50f966a4ea7e122d89e421da3130d83
Gleb Vasylenko <hleb.v@platinumlist.pro>
2022-06-09 13:28:47 +0300

    check_sum_from_str test

--- src/common.rs
+++ src/common.rs
@@ -10,3 +10,17 @@ pub(crate) fn check_sum_from_str(value: &str) -> usize {
 
     sum
 }
+
+#[cfg(test)]
+mod tests {
+    use super::check_sum_from_str;
+
+    #[test]
+    fn check_sum_from_str_test() {
+        let sum = check_sum_from_str("Hello World");
+        assert_eq!(1052, sum);
+
+        let sum = check_sum_from_str("Key");
+        assert_eq!(297, sum);
+    }
+}
